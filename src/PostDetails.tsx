45d6dafc0911b641eb48df88505943139a52d29e
GrayHead <ss.zhuravlov@gmail.com>
2021-09-20 20:59:53 +0300

    init

---
+++ src/PostDetails.tsx
@@ -0,0 +1,11 @@
+import {RouteComponentProps, useHistory} from "react-router";
+
+const PostDetails = (props: RouteComponentProps) => {
+
+    let {history, location, match} = props;
+    return <div>
+        post details
+    </div>
+}
+
+export default PostDetails;
